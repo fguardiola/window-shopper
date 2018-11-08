28d5c80dc16f1a7c9d810aee7e215823c15be415
fguardiola <fernando.guardiola.888@gmail.com>
2018-11-08 17:20:32 +0100

    Lesson 2. Set textProps and button UI tweaks

--- window-shopper/Controller/MainController.swift
+++ window-shopper/Controller/MainController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class MainController: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
     override func viewDidLoad() {
+       
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //create a button coding
+        let calcButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        
+        calcButton.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0.9142765411)
+        calcButton.setTitle("Calculate", for: .normal)
+        calcButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        // add an action function to button
+        calcButton.addTarget(self, action: #selector(MainController.calculate), for: .touchUpInside)
+        
+        //to make button appear just over te keyboard trick
+        wageTxt.inputAccessoryView = calcButton
+        wageTxt.inputAccessoryView = calcButton
     }
 
-
+    @objc func calculate(){
+        print("Button Clicked!!")
+    }
 }
 
--- window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/Views/CurrencyTextField.swift
@@ -7,22 +7,26 @@
 //
 
 import UIKit
-
+@IBDesignable
 class CurrencyTextField: UITextField {
+    override func prepareForInterfaceBuilder() {
+//        super.prepareForInterfaceBuilder()
+        self.setUp()
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+        self.setUp()
+    }
+    
+    func setUp(){
         backgroundColor = #colorLiteral(red: 0.9961728454, green: 0.9902502894, blue: 1, alpha: 0.2545483733)
         layer.cornerRadius = 5
         textAlignment = .center
-    
+        
         if let p = placeholder {
             self.attributedPlaceholder = NSAttributedString(string: p, attributes: [NSAttributedString.Key.foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
         }else {
-              self.attributedPlaceholder = NSAttributedString(string: "Text here", attributes: [NSAttributedString.Key.foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            self.attributedPlaceholder = NSAttributedString(string: "Text here", attributes: [NSAttributedString.Key.foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
         }
-      
-        
-
     }
 }
