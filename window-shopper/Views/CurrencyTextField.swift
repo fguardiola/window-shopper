ed2d561f8aed5a7074666e9078ed0a2736e6a60c
fguardiola <fernando.guardiola.888@gmail.com>
2018-11-08 16:24:33 +0100

    Lesson 1 - Basic UI setup

---
+++ window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,28 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by 67621177 on 08/11/2018.
+//  Copyright © 2018 67621177. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundColor = #colorLiteral(red: 0.9961728454, green: 0.9902502894, blue: 1, alpha: 0.2545483733)
+        layer.cornerRadius = 5
+        textAlignment = .center
+    
+        if let p = placeholder {
+            self.attributedPlaceholder = NSAttributedString(string: p, attributes: [NSAttributedString.Key.foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        }else {
+              self.attributedPlaceholder = NSAttributedString(string: "Text here", attributes: [NSAttributedString.Key.foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        }
+      
+        
+
+    }
+}
